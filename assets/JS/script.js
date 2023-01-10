77c2e3e288fac0249355f60600c333c77b59c1f4
enayatullah azizzada <e_azizzada@yahoo.com>
2023-01-10 16:24:53 -0500

    experiance section

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -15,3 +15,23 @@ window.onscroll = () => {
 	menu.classList.remove('bx-x');
 	navlist.classList.remove('open');
 };
+
+/*== Qualification tab ==*/
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content]')
+
+  tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+      const target = document.querySelector(tab.dataset.target)
+
+      tabContents.forEach(tabContent =>{
+        tabContent.classList.remove('qualification_active')
+      })
+      target.classList.add('qualification_active')
+
+      tabs.forEach(tab =>{
+        tab.classList.remove('qualification_active')
+      })
+      tab.classList.add('qualification_active')
+    })
+  })
